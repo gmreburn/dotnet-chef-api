9ec96d44159c35906a99fb10bd7028f2c713ef6a
gmreburn <gmreburn+git@gmail.com>
2016-10-13 20:23:03 -0500

    init chef server api client

---
+++ src/dotnet-chef-api/ChefServerApiClient.cs
@@ -0,0 +1,53 @@
+﻿namespace EBSCO.ChefServer
+{
+    using System;
+    using System.Net;
+    using RestSharp;
+
+    public class ChefServerApiClient
+    {
+        private readonly string user;
+        private readonly string pem;
+        private readonly RestClient client;
+        private const string orgTemplate = "/organizations/{0}/{1}";
+
+        public ChefServerApiClient(string client, string pem, Uri baseUri)
+        {
+            user = client;
+            this.pem = pem;
+            this.client = new RestClient(baseUri);
+        }
+
+        public ChefServerApiClient(string client, string pem, string baseUri) : this(client, pem, new Uri(baseUri))
+        {
+
+        }
+
+        public void FetchUsers()
+        {
+            var request = new AuthenticatedChefRequest(user, new Uri("/users", UriKind.Relative));
+            request.Sign(pem);
+            var response = client.Execute(request);
+
+            switch (response.StatusCode)
+            {
+                    case HttpStatusCode.OK:
+                    break;
+                    case HttpStatusCode.Unauthorized:
+                    
+                    break;
+                    case HttpStatusCode.Forbidden:
+                    break;
+                    case HttpStatusCode.NotFound:
+                    break;
+            }
+        }
+
+        public IRestResponse FetchRoles(string organization)
+        {
+            var request = new AuthenticatedChefRequest(user, new Uri(string.Format(orgTemplate, organization, "roles"), UriKind.Relative));
+            request.Sign(pem);
+            return client.Execute(request);
+        }
+    }
+}
---
+++ src/unittests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿namespace EBSCO.ChefServer.Tests
+{
+    using System;
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ChefServerApiClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var user = "gmreburn";
+            var baseUrl = new Uri("https://api.opscode.com");
+            var client = new ChefServerApiClient(user, File.ReadAllText("test.pem"), baseUrl);
+
+            // Act
+            var resp = client.FetchRoles("chef_rendez");
+
+            // Assert
+            Assert.AreEqual("11.4.0", resp);
+        }
+    }
+}
